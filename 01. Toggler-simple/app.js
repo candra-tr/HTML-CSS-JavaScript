25c4bd0253197c78e55f67fdeca8b9cec640f5b8
candra-tr <ctriatna@gmail.com>
2023-12-08 09:55:14 +0700

    Commit toggler

--- 01. Toggler-simple/app.js
+++ 01. Toggler-simple/app.js
@@ -1,13 +1,11 @@
 let toggler = document.getElementById("switch");
 
-toggler.addEventListener("click", () => {
+toggler.addEventListener("click", function () {
   //   if (toggler.checked === true) {
   //     document.body.style.backgroundColor = "black";
   //   } else {
   //     document.body.style.backgroundColor = "white";
   //   }
 
-  toggler.checked === true
-    ? (document.body.style.backgroundColor = "black")
-    : (document.body.style.backgroundColor = "white");
+  document.body.style.background = toggler.checked ? "black" : "white";
 });
