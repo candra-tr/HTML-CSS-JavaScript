da224399e3ceb0269a186b8da951cd51ddbbbf65
candra-tr <ctriatna@gmail.com>
2024-03-31 12:27:47 +0700

    add application recipe

---
+++ 10.  Forkify-recipe-application-custome/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4c292101-0a1e-472a-a3d6-829301864457';
+export const MODAL_CLOSE_SEC = 2.5;
